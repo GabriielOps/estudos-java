28b92af50e381e76d4cb21788109c22a97593ec1
Gabriel <gabriel_oliveira404@protonmail.com>
2024-11-28 10:35:35 -0300

    Add: Try on the scanner at main()

--- contador/src/contador.java
+++ contador/src/contador.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 public class contador {
 
     public static void main(String[] args) throws Exception {
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
+        try (Scanner terminal = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
 
-        try {
-            contar(parametroUm, parametroDois);
+            try {
+                contar(parametroUm, parametroDois);
 
-        } catch (ParametrosInvalidosException exception) {
-            System.out.println(exception.getMessage());
+            } catch (ParametrosInvalidosException exception) {
+                System.out.println(exception.getMessage());
+            }
         }
 
     }
